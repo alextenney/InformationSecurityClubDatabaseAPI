fe917b02f73940c04be4d0e7e2ec2d52c658a6c2
alextenney <44792738+alextenney@users.noreply.github.com>
2020-04-18 21:19:50 -0600

    added comment

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,4 +1,11 @@
-﻿using System;
+/* 
+Contributers: Alexandra Tenney, Martha Ibarra, Jeremy Stuart
+ Date Updated: April 17, 2020
+ Description: These are the controllers, that interact with the Front End (or in our case post man) ans receive the incoming HTTP 
+ Request paired wiht any input and then process that request by sending it to the Database Model
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,113 +22,121 @@ namespace ProjectTemp.Controllers
     {
 
         // GET api/InfoSecDB/adminSelectsParticipant
+        // grabs input pNaem and sends to Database Model
         [HttpGet]
         [Route("adminSelectsParticipant")]
         public ActionResult<IEnumerable<string>> adminSelectsParticipant([FromBody] JObject data)
         {
-            string pName = (string)data["pName"];
+            string pName = (string)data["pName"]; //creates a variable for the user input of pName
 
 
-            DatabaseModel dbm = new DatabaseModel();
-            DataTable dt = dbm.adminSelectsParticipant(pName);
+            DatabaseModel dbm = new DatabaseModel(); //creates a database model
+            DataTable dt = dbm.adminSelectsParticipant(pName); // calls the method in the database models
 
-            return Ok(dt);
+            return Ok(dt); //returns any values produced as a json
         }
 
 
         // GET api/InfoSecDB/GetChallengeInfo
+        //gets all challenges, no input given
         [HttpGet]
         [Route("GetChallengeInfo")]
         public ActionResult<IEnumerable<string>> GetChallengeInfo()
         {
             DatabaseModel dbm = new DatabaseModel(); // creates a database model
 
-            DataTable dt = dbm.GetChallengeInfo(); 
+            DataTable dt = dbm.GetChallengeInfo(); // calls the method in the database models
 
-            return Ok(dt);
+            return Ok(dt); //returns any values produced as a json
         }
 
         // GET api/InfoSecDB/GetTeam
+        // gets all participants on a team, teamName given as input 
         [HttpGet]
         [Route("GetTeam")]
         public ActionResult<IEnumerable<string>> GetTeam([FromBody] JObject data)
         {
-            string teamName = (string)data["teamName"];
+            string teamName = (string)data["teamName"]; //variable for the user input of teamName
             DatabaseModel dbm = new DatabaseModel(); // creates a database model
 
-            DataTable dt = dbm.GetTeam(teamName); 
+            DataTable dt = dbm.GetTeam(teamName); //calls the method GetTeam in the database Model
 
 
-            return Ok(dt);
+            return Ok(dt); //returns any values produced as a json
         }
 
         // PUT api/InfoSecDB/adminAddsTeamMember
+        //adds a participant already existing in a database to a team, participantName and teamName given as input
         [HttpPut]
         [Route("adminAddsTeamMember")]
         public ActionResult<IEnumerable<string>> adminAddsTeamMember([FromBody] JObject data)
         {
-            string participantName = (string)data["participantName"];
-            string teamName = (string)data["teamName"];
+            string participantName = (string)data["participantName"]; //variable for the user input of participantName
+            string teamName = (string)data["teamName"]; // variable for the user input of teamName
 
-            DatabaseModel dbm = new DatabaseModel();
-            int value = dbm.adminAddsTeamMember(participantName, teamName);
+            DatabaseModel dbm = new DatabaseModel(); // creates a new database model
+            int value = dbm.adminAddsTeamMember(participantName, teamName); // calls the database model, stored as an int
 
-            return Ok(value);
+            return Ok(value); //returns a positive integer for success, a negative integer for failed
         }
 
         // PUT api/InfoSecDB/participantAddsAttendance
+        // an exisitng participant adds attendance to themselves 
         [HttpPut]
         [Route("participantAddsAttendance")]
         public ActionResult<IEnumerable<string>> participantAddsAttendance([FromBody] JObject data)
         {
-            string participantName = (string)data["participantName"];
-            string meetingDate = (string)data["meetingDate"];
+            string participantName = (string)data["participantName"]; //variable for the user input of participantName
+            string meetingDate = (string)data["meetingDate"]; //variable for the user input of meetingDate
 
-            DatabaseModel dbm = new DatabaseModel();
-            int value = dbm.participantAddsAttendance(participantName, meetingDate);
+            DatabaseModel dbm = new DatabaseModel(); //creates a new database model
+            int value = dbm.participantAddsAttendance(participantName, meetingDate); // calls the database model, stored as an int
 
-            return Ok(value);
+            return Ok(value); //returns a positive integer for success, a negative integer for failed
         }
 
         // POST api/InfoSecDB/adminCreatesTeam
+        // takes a teamname as an input and creates a team (sends it to the stored procedure)
         [HttpPost]
         [Route("adminCreatesTeam")]
         public ActionResult<IEnumerable<string>> adminCreatesTeam([FromBody] JObject data)
         {
-            string teamName = (string)data["teamName"];
+            string teamName = (string)data["teamName"]; //variable for the user input of meetingDate
 
-            DatabaseModel dbm = new DatabaseModel();
-            int value = dbm.adminCreatesTeam(teamName);
+            DatabaseModel dbm = new DatabaseModel(); //creates a new database model
+            int value = dbm.adminCreatesTeam(teamName); // calls the database model, stored as an int
 
-            return Ok(value);
+            return Ok(value); //returns a positive integer for success, a negative integer for failed
         }
 
         // PUT api/InfoSecDB/participantAddsThemselvesToTeam
+        // participant gives input of their name and a team name to add themselves to that team
         [HttpPut]
         [Route("participantAddsThemselvesToTeam")]
         public ActionResult<IEnumerable<string>> participantAddsThemselvesToTeam([FromBody] JObject data)
         {
-            string participantName = (string)data["participantName"];
-            string teamName = (string)data["teamName"];
+            string participantName = (string)data["participantName"]; //variable for the user input of participantName
+            string teamName = (string)data["teamName"]; //variable for the user input of teamName
 
-            DatabaseModel dbm = new DatabaseModel();
-            int value = dbm.participantAddsThemselvesToTeam(participantName, teamName);
+            DatabaseModel dbm = new DatabaseModel(); //creates a new database model
+            int value = dbm.participantAddsThemselvesToTeam(participantName, teamName); // calls the database model, stored as an int
 
-            return Ok(value);
+            return Ok(value); //returns a positive integer for success, a negative integer for failed
         }
 
 
         // GET api/InfoSecDB/SelectCTF
+        //.
         [HttpGet]
         [Route("SelectCTF")]
         public ActionResult<IEnumerable<string>> SelectCTF([FromBody] JObject data)
         {
-            string ctfName = (string)data["ctfName"];
+            string ctfName = (string)data["ctfName"]; //variable for the user input of ctfName
 
-            DatabaseModel dbm = new DatabaseModel();
-            DataTable dt = dbm.SelectCTF(ctfName);
+            DatabaseModel dbm = new DatabaseModel(); //creates a new database model
+            DataTable dt = dbm.SelectCTF(ctfName); // calls the database model
 
-            return Ok(dt);
+            return Ok(dt);//returns any values produced as a json
         }
 
         // GET api/InfoSecDB/GetTeamInfo
@@ -129,37 +144,39 @@ namespace ProjectTemp.Controllers
         [Route("GetTeamInfo")]
         public ActionResult<IEnumerable<string>> GetTeamInfo([FromBody] JObject data)
         {
-            string teamName = (string)data["teamName"];
-            List<string> myP = new List<string>(); 
-            DatabaseModel dbm = new DatabaseModel(); 
+            string teamName = (string)data["teamName"]; //variable for the user input of teamName
+            List<string> myP = new List<string>();  
+            DatabaseModel dbm = new DatabaseModel(); //creates a new database model
 
-            DataTable dt = dbm.CTFTeam(teamName); 
-            return Ok(dt);
+            DataTable dt = dbm.CTFTeam(teamName);  // calls the database model
+            return Ok(dt);//returns any values produced as a json
         }
 
 
         // GET api/InfoSecDB/SelectWhoFoundFlag
+        // takes flag as input and calls the model 
         [HttpGet]
         [Route("SelectWhoFoundFlag")]
         public ActionResult<IEnumerable<string>> selectWhoFoundFlag([FromBody] JObject data)
         {
-            string flag = (string)data["flag"];
+            string flag = (string)data["flag"];//variable for the user input of flag
 
-            DatabaseModel dbm = new DatabaseModel();
-            DataTable dt = dbm.selectWhoFoundFlag(flag);
+            DatabaseModel dbm = new DatabaseModel();// creates a database model
+            DataTable dt = dbm.selectWhoFoundFlag(flag); // calls the database model
 
-            return Ok(dt);
+            return Ok(dt);//returns any values produced as a json
         }
 
         // GET api/InfoSecDB/SelectNotInTeam
+        // selects all participant not in a team
         [HttpGet]
         [Route("SelectNotInTeam")]
         public ActionResult<IEnumerable<string>> selectNotInTeam()
         { 
             DatabaseModel dbm = new DatabaseModel(); // creates a database model
-            DataTable dt = dbm.selectNotInTeam(); 
+            DataTable dt = dbm.selectNotInTeam();  // calls the database model
 
-            return Ok(dt);
+            return Ok(dt);//returns any values produced as a json
         }
 
     }
